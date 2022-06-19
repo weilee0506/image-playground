ae6c94bcf72c97f1b42c1a9e3206e09eb479fb79
weilee0506 <weilee19960506@gmail.com>
2022-06-19 23:42:41 +0800

    0619 commit-finish functionality

--- JPEGtoPNGconverter.py
+++ JPEGtoPNGconverter.py
@@ -7,10 +7,25 @@ import os
 from PIL import Image
 
 # grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+print(image_folder, output_folder)
 
 # check is new/ exists, if not create
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
 
 # loop through Pokedex
 # convert images to png
 # save to the new folder
+for filename in os.listdir(image_folder):
+    if "jpg" in filename:
+        img = Image.open(f"{image_folder}{filename}")
+        # 可以把檔名跟extention拆開變成tuple
+        # e.g. ('bulbasaur', '.jpg')
+        clean_filename = os.path.splitext(filename)
+        print(clean_filename)
+        img.save(f"{output_folder}{clean_filename[0]}.png","png")
+        print("all done")
+
 
